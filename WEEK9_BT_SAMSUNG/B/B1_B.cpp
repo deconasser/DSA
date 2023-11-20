dd666680ab85457607aa2aeffa6efcf0d38e8f9b
Nguyễn Bá Thịnh <124879074+deconasser@users.noreply.github.com>
2023-11-20 19:49:27 +0700

    up

---
+++ WEEK9_BT_SAMSUNG/B/B1_B.cpp
@@ -0,0 +1,2 @@
+Khoảng Cách Không Phản Ánh Thứ Tự Tô Pô.
+Thêm nữa, trong sắp xếp topo mỗi đỉnh phụ thuộc vào các đỉnh đến trước nó theo hướng của cạnh, BFS không đánh giá được mối quan hệ phụ thuộc này.
